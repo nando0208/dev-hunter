d11019acf433c2c7356a8435c67fed278db56507
Fernando Ferreira <fernando01ferreira@gmail.com>
2018-05-27 11:47:53 -0300

    Initial Commit

---
+++ Candidate-MinerTests/Candidate_MinerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Candidate_MinerTests.swift
+//  Candidate-MinerTests
+//
+//  Created by Fernando Ferreira on 27/05/18.
+//  Copyright © 2018 Fernando Ferreira. All rights reserved.
+//
+
+import XCTest
+@testable import Candidate_Miner
+
+class Candidate_MinerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
